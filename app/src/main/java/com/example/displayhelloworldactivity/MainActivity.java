dbad58abe4adf8a1653c3d6fd399e1dbf62134c7
Alesandro Hobayan <alesandrosfboo@gmail.com>
2024-09-23 20:11:52 +0800

    updating

--- app/src/main/java/com/example/displayhelloworldactivity/MainActivity.java
+++ app/src/main/java/com/example/displayhelloworldactivity/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.displayhelloworldactivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
+    Button act2, act3, act4, act5, act6, act7, act8, act9, act10;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        act2 = findViewById(R.id/btn_act2);
 
     }
 }
