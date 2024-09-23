aa442e483407786f987be0e6a44311b50eddfdee
Alesandro Hobayan <alesandrosfboo@gmail.com>
2024-09-23 22:56:17 +0800

    tapos na ung sa activity 2 na pag pinindot ung button mag ooutput ng hello world

--- app/src/main/java/com/example/displayhelloworldactivity/Activity2.java
+++ app/src/main/java/com/example/displayhelloworldactivity/Activity2.java
@@ -1,6 +1,8 @@
 package com.example.displayhelloworldactivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +10,19 @@ public class Activity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        TextView output;
+        Button click;
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_2);
+
+        output = findViewById(R.id.tv_outputhello);
+        click = findViewById(R.id.btn_Click);
+        click.setOnClickListener(v ->{
+            output.setText("Hello World!");
+        });
+
     }
 }
