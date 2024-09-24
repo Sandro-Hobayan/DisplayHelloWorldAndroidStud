00ff89853e614833edbdfc1ef417249aace3dc8b
Alesandro Hobayan <alesandrosfboo@gmail.com>
2024-09-24 10:38:31 +0800

    counter activity done

--- app/src/main/java/com/example/displayhelloworldactivity/Activity4.java
+++ app/src/main/java/com/example/displayhelloworldactivity/Activity4.java
@@ -1,24 +1,51 @@
 package com.example.displayhelloworldactivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.w3c.dom.Text;
+
 public class Activity4 extends AppCompatActivity {
+
+    TextView output;
+    EditText input;
+    Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_4);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        output = findViewById(R.id.tv_4output);
+        input = findViewById(R.id.et_4input);
+        submit = findViewById(R.id.btn_4submit);
+
+        submit.setOnClickListener(v ->{
+            counter();
         });
+
     }
+
+    private void counter() {
+        try {
+            int x = Integer.parseInt(input.getText().toString());
+            x++;
+                input.setText(String.valueOf(x));
+                output.setText("");
+        } catch (NumberFormatException e) {
+            output.setText("Please enter a valid number.");
+        }
+    }
+
+
 }
