4cae85bed298982486550b21ef99cb04cba1b330
Alesandro Hobayan <alesandrosfboo@gmail.com>
2024-09-24 14:54:30 +0800

    user age activity or activity 6 done

--- app/src/main/java/com/example/displayhelloworldactivity/Activity6.java
+++ app/src/main/java/com/example/displayhelloworldactivity/Activity6.java
@@ -1,24 +1,47 @@
 package com.example.displayhelloworldactivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Calendar;
 
 public class Activity6 extends AppCompatActivity {
+
+    TextView output;
+    EditText input;
+    Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_6);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        output = findViewById(R.id.tv_6output);
+        input = findViewById(R.id.et_6input);
+        submit = findViewById(R.id.btn_6submit);
+
+        submit.setOnClickListener(v ->{
+            toGetAge();
         });
+
+    }
+
+    private void toGetAge(){
+        try {
+            int birthYear = Integer.parseInt(input.getText().toString());
+            int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+            int age = currentYear - birthYear;
+
+            output.setText(age + " Years of old");
+
+        }catch (Exception e){
+            output.setText("Put your birth year (e.g., 1990)");
+        }
     }
 }
