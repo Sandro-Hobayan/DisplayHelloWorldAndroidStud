6af0b5d43bf446ade6a6b661490632109404a947
Alesandro Hobayan <alesandrosfboo@gmail.com>
2024-09-24 13:51:41 +0800

    simple form or activity 5 done at gamay na den ung methods

--- app/src/main/java/com/example/displayhelloworldactivity/Activity5.java
+++ app/src/main/java/com/example/displayhelloworldactivity/Activity5.java
@@ -1,6 +1,11 @@
 package com.example.displayhelloworldactivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,75 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Activity5 extends AppCompatActivity {
 
+    TextView outputgen, outputname, outputage;
+    EditText name, age;
+    RadioButton rmale, rfemale;
+    RadioGroup rbgroup;
+    Button submit, clear;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_5);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        outputgen = findViewById(R.id.tv_5gen);
+        outputname = findViewById(R.id.tv_5name);
+        outputage = findViewById(R.id.tv_5age);
+        name = findViewById(R.id.et_5name);
+        age = findViewById(R.id.et_5age);
+        rmale = findViewById(R.id.rb_5male);
+        rfemale = findViewById(R.id.rb_5female);
+        rbgroup = findViewById(R.id.rdg_5rbgroup);
+
+        submit = findViewById(R.id.btn_5submit);    //submit button
+        submit.setOnClickListener(v ->{
+            try {
+                formOutput();
+            }catch (Exception e){
+                outputname.setText("You must input data first");
+            }
         });
+
+        clear = findViewById(R.id.btn_5clear);  //clear button
+        clear.setOnClickListener(v ->{
+            outputgen.setText("");
+            outputname.setText("");
+            outputage.setText("");
+            name.setText("");
+            age.setText("");
+            rbgroup.setSelected(false);
+        });
+
+    }
+
+    private void formOutput(){
+        //set if male or female
+
+        String gender;
+        if(rfemale.isChecked()){
+            gender = "Female";
+        }else{
+            gender = "Male";
+        }
+
+        //set generation by age
+        int agerange = Integer.parseInt(age.getText().toString());
+        String setgeneration;
+        if(agerange < 12){
+            setgeneration = "Young";
+        }else if(agerange <= 18){
+            setgeneration = "Teen";
+        }else if(agerange <= 50){
+            setgeneration = "Adult";
+        }else{
+            setgeneration = "Old";
+        }
+        String nameValue = name.getText().toString();
+
+        outputgen.setText(setgeneration+ " " +gender);
+        outputname.setText(nameValue);
+        outputage.setText(age.getText().toString());
+
     }
 }
