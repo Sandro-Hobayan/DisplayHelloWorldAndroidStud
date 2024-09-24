a827cbe42146622775e6fab8bc25d5552ceece82
Alesandro Hobayan <alesandrosfboo@gmail.com>
2024-09-24 17:15:15 +0800

    Odd and Even checker done

--- app/src/main/java/com/example/displayhelloworldactivity/Activity8.java
+++ app/src/main/java/com/example/displayhelloworldactivity/Activity8.java
@@ -1,24 +1,41 @@
 package com.example.displayhelloworldactivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class Activity8 extends AppCompatActivity {
+
+    TextView output;
+    EditText input;
+    Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_8);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        output = findViewById(R.id.tv_8output);
+        input = findViewById(R.id.et_8input);
+        submit = findViewById(R.id.btn_8submit);
+
+        submit.setOnClickListener(v ->{
+            oddEvenChecker();
         });
+    }
+    public void oddEvenChecker(){
+        int num = Integer.parseInt(input.getText().toString());
+        if(num % 2 == 0){
+            String result = "even";
+            output.setText(num + " is " + result);
+        }else{
+            String result = "odd";
+            output.setText(num + " is " + result);
+        }
     }
 }
