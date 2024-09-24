d1cd21ee6b69b421e09de486157b916d92868529
Alesandro Hobayan <alesandrosfboo@gmail.com>
2024-09-24 20:38:37 +0800

    palindrome activity doneeeeee

--- app/src/main/java/com/example/displayhelloworldactivity/Activity10.java
+++ app/src/main/java/com/example/displayhelloworldactivity/Activity10.java
@@ -1,24 +1,41 @@
 package com.example.displayhelloworldactivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class Activity10 extends AppCompatActivity {
+
+    TextView output;
+    EditText input;
+    Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_10);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        output = findViewById(R.id.tv_10output);
+        input = findViewById(R.id.et_10input);
+        submit = findViewById(R.id.btn_10submit);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userInput = input.getText().toString().toLowerCase();                        //convert wodrs to lowercase
+                String reversedInput = new StringBuilder(userInput).reverse().toString();
+
+                if (userInput.equals(reversedInput)) {
+                    output.setText("It's a palindrome!");
+                } else {
+                    output.setText("Not a palindrome.");
+                }
+            }
         });
     }
 }
